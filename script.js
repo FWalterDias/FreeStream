67a06c51c9de60099a1b3ca3ca842f028ab6a159
Francisco Walter <franciscowalterd@gmail.com>
2023-03-17 14:42:04 -0300

    script inicial adicionado

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const main = document.querySelector('#main');
+const shows = document.querySelector('.showsAberto');
+const cinema = document.querySelector('.cinemaAberto');
+const teatro = document.querySelector('.teatroAberto');
+
 function exibeShows(){
-    alert('oi');
+    main.style.display = 'none';
+    shows.style.display = 'block';
+}
+function exibeCinema(){
+    main.style.display = 'none';
+    cinema.style.display = 'block';
+}
+function exibeTeatro(){
+    main.style.display = 'none';
+    teatro.style.display = 'block';
+}
+function voltar(){
+    main.style.display = 'grid';
+    shows.style.display = 'none';
+    cinema.style.display = 'none';
+    teatro.style.display = 'none';
 }
